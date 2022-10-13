f98b4989331ce7816e9951d359862636eb94a15e
JoseAugusto7 <tails.augusto@hotmail.com>
2022-10-12 21:05:14 -0300

    iniciando

---
+++ boostrap 5/Logica_py/Aula1.py
@@ -0,0 +1,16 @@
+# Variáveis =
+# números
+velociade_internet = 10
+print(velociade_internet)
+# valores boleanos
+esta_aberto = True
+# Strings
+nome_do_curso = "Lógica de programação"
+
+# Como variáveis seriam usadas em um programa real?
+# Problema 1: Escreva o valor que um funcionário ganha por hora com base no seu salário mensal, em horas trabalhadas por mês.
+
+salario_mensal = input("Qual é o seu salário mensal?")
+horas_trabalhadas_mensalmente = input("Quantas horas você trabalha por mês?")
+valor_hora = int(salario_mensal) / int(horas_trabalhadas_mensalmente)
+print (valor_hora)
---
+++ boostrap 5/Logica_py/Aula2.py
@@ -0,0 +1,12 @@
+# Condicionais
+# if, elif e else
+
+# Problema 2: Encontre o maior entre dois números.
+
+primeiro_valor = input("Digite o primeiro valor!")
+segundo_valor = input("Digite o segundo valor!")
+
+if int(primeiro_valor) > int(segundo_valor):
+    print("O primeiro valor é maior.")
+else:
+    print("O segundo valor é maior.")
